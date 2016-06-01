9a42e1b1a4de46161a216e827fb48adaabb2a490
hoter <aytpaev@gmail.com>
2016-06-01 15:46:28 +0000

    HTML5 templates

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,6 +5,9 @@
         <title><?php print $head_title; ?></title>
         <?php print $styles; ?>
         <?php print $scripts; ?>
+        <!--[if lt IE 9]>
+          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
     </head>
     <body class="<?php print $classes; ?>">
       <?php print $page_top; ?>
