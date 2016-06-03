a1a9350e39cf12b0e9c9355d3ccce7ecdfcf4b8e
hoter <aytpaev@gmail.com>
2016-06-03 16:32:22 +0000

    Stylize some stuctures

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -1,3 +1,17 @@
 <div id="page">
+    <section>
+        <?php print render($page['content']); ?>
+    </section>
+
+    <?php if ($page['header']): ?>
+        <header>
+            <?php print render($page['header']); ?>
+        </header>
+    <?php endif; ?>
     
+    <?php if ($page['footer']): ?>
+        <footer>
+            <?php print render($page['footer']); ?>
+        </footer>
+    <?php endif; ?>
 </div>
